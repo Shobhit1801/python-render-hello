6fc5ee81411d2719f2597726414b2286ebe1378c
abhishek <garg.abhishek07@gmail.com>
2025-10-07 13:53:14 +0530

    Initialise client to other place - prev

--- supabase_client.py
+++ supabase_client.py
@@ -20,7 +20,7 @@ def fetch_supabase_db(client_id):
 
 def fetch_supabase_cat_db():
   # Initialize client
-  
+  supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
   response = supabase.table('categories').select('*').execute()
   if response.data:
